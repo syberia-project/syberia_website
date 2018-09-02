c09af1ac0c65df617db519056d0a88e78eaad04e
Lane Shukhov <rl.shukhov@mail.ru>
2018-09-02 20:50:56 +0300

    Update site stub:  - go to html5boilerplate frontend  - go to fatfree templates  - add .gitignore  - add icons  - add robots.txt  - add humans.txt  - add custom Error Page  - some fixes

--- index.php
+++ index.php
@@ -4,15 +4,30 @@
  * Date: 27.08.2018
  * Time: 0:15
  */
-include_once "config.php";
+include_once 'config.php';
 
-define("SITE_URL", 'https://syberiaos.com/');
+define('SITE_URL', 'https://syberiaos.com/');
 
-$f3->set('DEBUG',0);
+$f3->set('DEBUG', 0);
 $f3->set('site_url', SITE_URL);
 
-$f3->route('GET /', function($f3) {
-    echo \Template::instance()->render('stub.html');
-});
+function renderPage($f3, $title, $page) {
+    $f3->set('page_title', $title);
+    $f3->set('page_content', $page);
+    echo \Template::instance()->render('layout.html');
+}
+
+$indexAction = function ($f3) {
+    renderPage($f3, 'Syberia OS', 'pages/index.html');
+};
+
+$errorAction = function ($f3) {
+    echo \Template::instance()->render('error.html');
+};
+
+$f3->set('ONERROR', $errorAction);
+
+$f3->route('GET /', $indexAction);
 
 $f3->run();
+
