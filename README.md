351e0b4fc8b7d269a245b6bc4296fa9a8b443497
Lane Shukhov <rl.shukhov@mail.ru>
2018-10-01 00:08:36 +0300

    Syberia site is up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Syberia Website
 Official download portal of syberia project
+
+* https://github.com/bcosca/fatfree
+* https://github.com/h5bp/html5-boilerplate
+* https://github.com/FezVrasta/bootstrap-material-design
+* https://github.com/primer/octicons/
--- index.php
+++ index.php
@@ -12,6 +12,8 @@ $f3->set('DEBUG', 0);
 $f3->set('site_url', SITE_URL);
 
 function renderPage($f3, $title, $page) {
+    // TODO: dynamic navigation generation
+    // TODO: localisation support
     $f3->set('page_title', $title);
     $f3->set('page_content', $page);
     echo \Template::instance()->render('layout.html');
@@ -22,12 +24,29 @@ $indexAction = function ($f3) {
 };
 
 $errorAction = function ($f3) {
+    // TODO: redesign error page
     echo \Template::instance()->render('error.html');
+};
+
+$downloadsAction = function ($f3) {
+    // TODO: parse OTAs configs for get download links
+    renderPage($f3, 'Downloads - Syberia OS', 'pages/downloads.html');
+};
+
+$teamAction = function ($f3) {
+    renderPage($f3, 'Team - Syberia OS', 'pages/team.html');
+};
+
+$linksAction = function ($f3) {
+    renderPage($f3, 'Links - Syberia OS', 'pages/links.html');
 };
 
 $f3->set('ONERROR', $errorAction);
 
-$f3->route('GET /', $indexAction);
+$f3->route('GET /',          $indexAction);
+$f3->route('GET /downloads', $downloadsAction);
+$f3->route('GET /team',      $teamAction);
+$f3->route('GET /links',     $linksAction);
 
 $f3->run();
 
